286524de79bd2db498e437a5ac38dd8d2aaaa38e
Aki Saito <asaito@Akis-MacBook-Pro.local>
2020-02-27 19:46:22 -0800

    First commit

---
+++ src/build/precache-manifest.f88b325b3b4d5f4ac06399d6591d7279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f267659065fab9c8615ec7dab6f19354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e80569dc68999e6598f",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "4e328d2a211eebc3fc66",
+    "url": "/static/js/2.d8784924.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d8784924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e80569dc68999e6598f",
+    "url": "/static/js/main.9dd4ec3f.chunk.js"
+  },
+  {
+    "revision": "c9887e208502b1cb59e5",
+    "url": "/static/js/runtime-main.120a5705.js"
+  }
+]);
